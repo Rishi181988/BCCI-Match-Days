8819f490106db7a8ed2c6e9a24c8b732e838f778
Rishi181988 <124787348+Rishi181988@users.noreply.github.com>
2025-09-26 19:45:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,13 @@
 import streamlit as st
+import pandas as pd
 
-st.title('🎈 App Name')
+st.title('🎈 BCCI Match Data')
 
-st.write('Hello world!')
+st.write('This app compile all the BCCI domestic matches in one place')
+
+with st.expander('Data'):
+  st.write('**Raw Data**')
+
+# Data Preparations
+with st.sidebar:
+  st.header('Input Features')
